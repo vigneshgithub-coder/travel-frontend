c2df775c4de96b1935340cc2edf0bb4f202358b5
Vigneshwaran <vigneshwaranmca22@gmail.com>
2024-12-17 19:49:44 +0530

    Initial commit for frontend

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = () => {
   // Fetch data from backend
   useEffect(() => {
     axios
-      .get('hhttps://travel-backend-8.onrender.com/api')
+      .get('https://travel-backend-8.onrender.com/api')
       .then((response) => setPackages(response.data))
       .catch((error) => console.error('Error fetching data:', error));
   }, []);
